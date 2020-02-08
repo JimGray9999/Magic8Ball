644fdca25b6446bfbf05a63a4e9a52e8b014de23
Jim Gray <jimgray2005@gmail.com>
2020-02-08 08:31:30 -0500

    Setup project

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int choice;
+
+	    String[] outcomes = new String[] {"It is certain.",
+            // affirmative outcomes:
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            // non-committal outcomes:
+            "Reply hazy, try again.",
+            "Ask again later.",
+
+
+        };
+    }
+}
